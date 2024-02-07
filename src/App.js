1a7baf06d58b69a86d30c60c86216b0b31bcd13e
Shubham Sourav <119451511+shubham2000pm@users.noreply.github.com>
2024-02-07 11:13:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Home from "./components/Home/Home";
-import Success from "./components/success";
+import Success from "./components/Success";
 import Category from "./components/Category/Category";
 import SingleProduct from "./components/SingleProduct/SingleProduct";
 import Newsletter from "./components/Footer/Newsletter/Newsletter";
